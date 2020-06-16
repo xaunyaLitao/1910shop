079917e16f46342285ad37c38e9731cb74714b32
dante <1651928858@qq.com>
2020-06-16 11:41:16 +0800

    商品详情

---
+++ app/Http/Controllers/Goods/GoodsController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\Goods;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Model\GoodsModel;
+class GoodsController extends Controller
+{
+//    详情页
+    public function detail()
+    {
+    $goods_id=$_GET['id'];  //接收url的get参数
+        echo 'goods_id:'. $goods_id;echo '</br>';
+
+        // 查询商品详情
+        $info= GoodsModel::where(['goods_id'=>$goods_id])->first()->toArray();
+        echo '<pre>';print_r($info);echo '</pre>';
+
+    }
+}
